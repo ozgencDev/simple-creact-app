a6abbb9c42da0d7ebbfce7727b30c5a9506ae333
Ali <ozgencdev@gmail.com>
2021-10-27 23:06:27 +0300

    Last

--- createProject.js
+++ createProject.js
@@ -13,35 +13,57 @@ function createFile(filePath, filename = "bash.sh") {
 
 function creact() {
   const { INIT, PRRD, CHILDS, FILEPERCHILDS } = process.env;
+
   const srcChilds = CHILDS.split(",");
+
   const srcChildFiles = FILEPERCHILDS.split(",");
+
   for (let i = 1; i < i + 1; i++) {
     const finalPath = `${projectPath}-${i}`;
     const isExists = existsSync(finalPath);
+
     if (!isExists) {
+      console.log("Generating file structure");
+
       mkdirSync(path.join(finalPath, "src"), { recursive: true });
+
       srcChilds.forEach((val, index) => {
         mkdirSync(path.join(finalPath, "src", val));
+
         writeFileSync(
           path.join(finalPath, "src", val, srcChildFiles[index]),
           fileContent[val]
         );
       });
+
       writeFileSync(createFile(finalPath, "README.md"), fileContent["readme"]);
+
       writeFileSync(createFile(finalPath), INIT);
+
       execSync(`sh ${createFile(finalPath)}`, {
         cwd: finalPath,
       });
+
+      console.log(
+        "Dependencies may take a long time to install please wait..."
+      );
+
       writeFileSync(createFile(finalPath), PRRD);
+
       execSync(`sh ${createFile(finalPath)}`, {
         cwd: finalPath,
       });
+
       writeFileSync(
         createFile(finalPath, ".gitignore"),
         ".parcel-cache/\ndist/\nnode_modules/"
       );
 
       rmSync(createFile(finalPath), { force: true });
+
+      setTimeout(() => {
+        console.log("Process complete");
+      }, 2000);
 
       return;
     }
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 const { program } = require("commander");
+const open = require("open");
 
 const creact = require("./createProject");
 
 program.description("Start a clean and simple React Project");
 program.name("creact");
 program.helpOption(false);
-
 program.action(creact);
 
 program.parse(process.argv);
